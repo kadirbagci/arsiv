a630b027b8a46f31676e05bbbbf5adffea565311
kadirbagci <kadir.bagci@std.yildiz.edu.tr>
2025-11-13 00:58:22 +0300

    Update script.js

--- script.js
+++ script.js
@@ -256,5 +256,5 @@ document.addEventListener("DOMContentLoaded", () => {
         "Çocukluk fotoğraflarımıza bakarken çok benzeyen iki foğrafı buluşumuz ve senin yıldızlı mesajlarındaki duruşu"
     );
      
-container.append(card1, card2, card3, card4, card5, card6, card7, card8, card9, card10, card11, card12, card13, card14, card15, card16, card17, card18, card19, card20, card21, card22, card23, card24, card25, card26, card27, card28, card29, card30);
+container.append(card1, card2, card3, card4, card5, card6, card7, card8, card9, card10, card11, card12, card13, card14, card15, card16, card17, card18, card19, card20, card21, card22, card23, card24, card25, card26, card27, card28, card29, card30, card31, card32);
 });
